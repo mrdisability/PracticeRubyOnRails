c60ed3e2ff8e0b786a08f02d81f38aded999a51b
King Ioane <thesamoanppprogrammer@gmail.com>
2022-01-13 01:59:34 +1300

    Able to use rails c but cant use require 'mysql2'

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'home/prices'
+
+  resources :todos, only: [:index, :show, :create, :update, :destroy]
  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
