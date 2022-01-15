0782afc514880c2d8af261506ac269960657d30f
King Ioane <thesamoanppprogrammer@gmail.com>
2022-01-15 23:04:55 +1300

    Added origin cors policy for vue frontend

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins 'http://localhost:3001'
-        resource '*', headers: :any, methods: [:get, :post]
+        origins 'http://localhost:3001', 'http://localhost:8080'
+        resource '*', headers: :any, methods: [:get, :post, :delete, :put]
     end
 end
