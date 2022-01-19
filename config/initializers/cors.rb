c8547b04626c5a69a5e8fad0b5f85efcd659ec1b
King Ioane <thesamoanppprogrammer@gmail.com>
2022-01-19 13:48:45 +1300

    Added http://localhost:4200 to allow angular website to consume api.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins 'http://localhost:3001', 'http://localhost:8080'
+        origins 'http://localhost:3001', 'http://localhost:8080', 'http://localhost:4200'
         resource '*', headers: :any, methods: [:get, :post, :delete, :put]
     end
 end
